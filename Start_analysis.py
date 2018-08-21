b1ae9133939ca351bfec03339ce2a312d0adccf1
Federico Claudi <federicoclaudi@gmail.com>
2018-08-21 16:15:00 +0100

    Organised Analysis() class with subfuncs to improve readability

--- Start_analysis.py
+++ Start_analysis.py
@@ -9,34 +9,64 @@ from Processing import Processing_main
 from Config import load_database, update_database, load_name, save_name\
     , selector_type, selector,\
     extract_background, track_mouse, track_options, \
-    plotting, Cohort, processing
+    plotting, cohort, processing
+
 
 # The analysis is all contained within this class
 class Analysis():
     def __init__(self):
-        # Get the paths for folder where to load and save stuff
-        paths = load_paths()
-        save_fld = paths['save fld']
-        datalog_path = paths['datalog']
+        """
+        Set up the analysis:
+        - get global vars
+        - load database
+        - call relevant functions [e.g. tracking, plotting...]
+        """
 
-        # Keep track if TF is set up for DLC analysis
+        # Get path used throughout the analysis
+        paths = load_paths()
+        self.save_fld = paths['save fld']
+        self.datalog_path = paths['datalog']
+
+        # Flags to keep track if TF is set up for DLC analysis
         self.TF_setup = False
         self.TF_sttings = None
 
+        # Load the database
+        self.load_database()
+
+        # Call the main funct that orchestrates the application of the subprocesses
+        self.main()
+
+        # Save the final results
+        self.save_results(obj=self.db, mod='_completed')
+
+    def load_database(self):
+        """
+        Take care of creating a new database from scratch, using the metadata in the datalog.csv file, or
+        loading a pre-existing database
+        """
+
         # Load database
         if load_database:
-            db = load_data(save_fld, load_name)
+            db = load_data(self.save_fld, load_name)
             # Update database with recently added sessions
             if update_database:
-                db.sessions = get_sessions_metadata_from_yaml(datalog_path, database=db)
+                db.sessions = get_sessions_metadata_from_yaml(self.datalog_path, database=db)
         else:
             # Create database from scratch
-            sessions_metadata = get_sessions_metadata_from_yaml(datalog_path)
-            db = generate_database_from_metadatas(sessions_metadata)
+            sessions_metadata = get_sessions_metadata_from_yaml(self.datalog_path)
+            self.db = generate_database_from_metadatas(sessions_metadata)
 
+    def main(self):
+        """"
+        Once all is set up we apply sub-processes to individual sessions or cohorts
+        """
+        ###############################################################################################################
+        ## WORK ON SINGLE COHORTS
+        ###############################################################################################################
         # Loop over all the sessions and call all the relevant functions [ For the sessions that need to be processed]
-        for session_name in sorted(db.index):
-            session = db.loc[session_name]
+        for session_name in sorted(self.db.index):
+            session = self.db.loc[session_name]
             # Check if this is one of th sessions we should be processing
             if not selector_type == 'any':
                 if selector_type == 'experiment' and session.Metadata.experiment not in selector:
@@ -48,45 +78,60 @@ class Analysis():
 
             print('---------------\nProcessing session {}'.format(session_name))
 
-            # extract info from sessions videos [e.g. first frame, fps, videos lenth...]
             if extract_background or track_mouse:
-                session = get_session_videodata(session)
+                self.video_analysis(session)
+
+            if processing:
+                self.processing_analysis(session)
+
+            if plotting:  # individuals - work in progress
+                self.plotting_analysis(session)
+
+            if cohort:
+                self.cohort_analysis()
+                if plotting:
+                    Plotting_main.setup_plotting(None, self.db, selector='cohort')
+
+    def video_analysis(self, session):
+            # extract info from sessions videos [e.g. first frame, fps, videos lenth...]
+            session = get_session_videodata(session)
 
             # Process background: get maze edges and user selected ROIs
             if extract_background:
+                # Get bg and save
                 maze_edges, user_rois = Image_processing.process_background(session['Video']['Background'],
                                                                             track_options)
                 session['Video']['Maze Edges'] = maze_edges
                 session['Video']['User ROIs'] = user_rois
-                save_data(save_fld, save_name, name_modifier='_background', object=db)
+
+                self.save_results(obj=self.db, mod='_bg')
 
             # Track animal on videos   <---!!!!
             if track_mouse:
-                tracked = Tracking(session, db, self.TF_setup, self.TF_sttings)
-                db = tracked.database
+                tracked = Tracking(session, self.db, self.TF_setup, self.TF_sttings)
+                self.db = tracked.database
                 self.TF_setup = tracked.TF_setup
                 self.TF_sttings = tracked.TF_settings
-                save_data(save_fld, save_name, name_modifier='_tracking', object=db)
 
+                self.save_results(obj=self.db, mod='_tracking')
+
+    def processing_analysis(self, session):
             # Processing
-            if processing:
-                Processing_main.Processing(session, db)
+            Processing_main.Processing(session, self.db)
 
+            self.save_results(obj=self.db, mod='_processing')
+
+    def plotting_analysis(self, session):
             # Plot for individual mice
-            if plotting:
-                Plotting_main.setup_plotting(session, db)
+            Plotting_main.setup_plotting(session, self.db)
 
-        if Cohort:
-            # Create a cohort and store it in database
-            db = create_cohort(db)  # Get all the trial data in one place
+    def cohort_analysis(self):
+        # Create a cohort and store it in database
+        self.db = create_cohort(self.db)  # Get all the trial data in one place
+        self.save_results(obj=self.db, mod='_cohort')
 
-            print('Processing Cohort')
-            # Plot for a cohort
-            if plotting:
-                Plotting_main.setup_plotting(None, db, selector='cohort')
-
-        # Save results at the end of the analysis
-        save_data(save_fld, save_name, object=db, name_modifier='_completed')
+    def save_results(self, obj=None, mod=None):
+        save_data(self.save_fld, save_name, object=obj, name_modifier=mod)
 
 
 #######################
