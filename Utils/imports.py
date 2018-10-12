0e17abac7b47c95037ce0e917ca4b8505c2d0474
FedeClaudi <federicoclaudi@gmail.com>
2018-10-12 13:35:31 +0100

    processing refactoring

--- Utils/imports.py
+++ Utils/imports.py
@@ -48,7 +48,9 @@ from Utils.Messaging import slack_chat_messenger
 from Utils.utils_classes import Trial, Cohort
 
 # Processing
-from Processing.Processing_maze import mazeprocessor,  mazecohortprocessor  # , ProcessingTrialsMaze, ProcessingSessionMaze
+from Processing.Processing_maze import mazeprocessor
+from Processing.Processing_maze_cohort import mazecohortprocessor
+from Processing.Processing_maze_expspecific import *
 from Processing.Processing_utils import *
 from multiprocessing.dummy import Pool as ThreadPool
 
